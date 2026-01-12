d81b425939dca1523b48eb1548d5619f47e4e690
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2026-01-12 11:51:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<div align="center"> 
+    <img src="./Cecilia_Perdomo.png" alt="Cecilia Perdomo" />
+</div>
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div>
+      <h1>Mi primera Landing Page con React</h1>
+    </div>
+  );
+}
+export default App;
